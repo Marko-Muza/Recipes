cdbaee9eeb73945fe0603347cc65fc347e341ad2
Marko-Muza <djuroperox@yahoo.com>
2020-11-15 11:44:58 +0100

    Shoping list, ingredient model, recipe detail.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg')
+    new Recipe('A Test Recipe','This is simply a test','https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg'),
+    new Recipe('A Test Recipe','This is simply a test','https://www.peta.org/wp-content/uploads/2020/04/Blank-1200-x-600-1.jpg')
   ];
 
   constructor() { }
