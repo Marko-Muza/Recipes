f41555d08f26865e7beacd6959d4df047f03c1b5
Marko-Muza <djuroperox@yahoo.com>
2021-05-28 09:09:43 +0200

    Recipe Service.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 //import { EventEmitter } from 'events';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,18 +10,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg'),
-    new Recipe('A Test Recipe','This is simply a test','https://www.peta.org/wp-content/uploads/2020/04/Blank-1200-x-600-1.jpg')
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+  
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { ThrowStmt } from "@angular/compiler";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://imageresizer.static9.net.au/vgvJq5iRXqIq6AjOxmeRUS9MNIA=/1000x563/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2016%2F09%2F09%2F11%2F11%2FPizza-Siciliana-eggplant-sausage-cherry-tomatoes.jpg',
+            [
+                new Ingredient('Sausages', 1),
+                new Ingredient('Tomato', 2)
+            ]),
+        new Recipe('A Test Recipe',
+        'This is simply a test',
+        'https://www.peta.org/wp-content/uploads/2020/04/Blank-1200-x-600-1.jpg',
+        [
+                new Ingredient('Potatoes', 6),
+                new Ingredient('Green beans', 2)
+        ])
+      ];
+      constructor(private slService: ShoppingListService) {
+
+      }
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
+      addIngredientsToShoppingList( ingredients: Ingredient[]) {
+          this.slService.addIngredients(ingredients);
+      }
+}
