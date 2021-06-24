be841ca3952700f9e8ceed08adb1d41e1707c273
Marko-Muza <djuroperox@yahoo.com>
2021-06-24 13:34:59 +0200

    Fetching recipes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,25 +9,32 @@ export class RecipeService {
     // Event:
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe',
-            'This is simply a test',
-            'https://imageresizer.static9.net.au/vgvJq5iRXqIq6AjOxmeRUS9MNIA=/1000x563/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2016%2F09%2F09%2F11%2F11%2FPizza-Siciliana-eggplant-sausage-cherry-tomatoes.jpg',
-            [
-                new Ingredient('Sausages', 1),
-                new Ingredient('Tomato', 2),
-            ]),
-        new Recipe('A Test Recipe',
-        'This is simply a test',
-        'https://www.peta.org/wp-content/uploads/2020/04/Blank-1200-x-600-1.jpg',
-        [
-                new Ingredient('Potatoes', 6),
-                new Ingredient('Green beans', 2),
-        ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe',
+    //         'This is simply a test',
+    //         'https://imageresizer.static9.net.au/vgvJq5iRXqIq6AjOxmeRUS9MNIA=/1000x563/https%3A%2F%2Fprod.static9.net.au%2F_%2Fmedia%2F2016%2F09%2F09%2F11%2F11%2FPizza-Siciliana-eggplant-sausage-cherry-tomatoes.jpg',
+    //         [
+    //             new Ingredient('Sausages', 1),
+    //             new Ingredient('Tomato', 2),
+    //         ]),
+    //     new Recipe('A Test Recipe',
+    //     'This is simply a test',
+    //     'https://www.peta.org/wp-content/uploads/2020/04/Blank-1200-x-600-1.jpg',
+    //     [
+    //             new Ingredient('Potatoes', 6),
+    //             new Ingredient('Green beans', 2),
+    //     ])
+    //   ];
+
+    private recipes: Recipe[] = [];
       constructor(private slService: ShoppingListService) {
 
+      }
+
+      setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
       }
 
       getRecipes() {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map } from 'rxjs/operators';
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({providedIn: 'root'}) // Because we inject service into service. I can use this shortcut {providedIn: 'root'} or i can provided it in app.module.ts
 export class DataStorageService {
@@ -11,6 +13,18 @@ export class DataStorageService {
         this.http.put('https://ng-recipe-book-fb329-default-rtdb.firebaseio.com/recipes.json', recipes)
         .subscribe(response => {
             console.log(response);
+        })
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>('https://ng-recipe-book-fb329-default-rtdb.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            })
+        }))
+        .subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
         })
     }
 }
